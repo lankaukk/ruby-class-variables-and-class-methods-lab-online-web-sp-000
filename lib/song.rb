16dab5296b2d93373b442c77a5c6623372521637
McKayla Lankau <lankaukk@yahoo.com>
2020-09-02 22:12:08 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,60 @@
+class Song 
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist 
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+  
+  @@count = 0 
+  @@artists = [] 
+  @@genres = []
+  
+  def self.count
+    @@count
+  end
+  
+  def name=(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
+  
+  def artist=(artist)
+    @artist = artist
+  end
+ 
+  def artist
+    @artist
+  end
+  
+  def genre=(genre)
+    @genre = genre
+  end
+ 
+  def genre
+    @genre
+  end
+  
+  def self.artists 
+    return @@artists
+  end
+  
+  def self.genres 
+    return @@genres
+  end 
+  
+  def self.artist_count 
+    
+  end 
+  
+  def self.genre_count 
+    
+  end
+  
+end
